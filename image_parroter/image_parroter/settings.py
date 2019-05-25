72f8e8390c828d6dd960760d4cb03cb51f37fd4e
Adam Mcquistan <amcquistan@unomaha.edu>
2019-05-24 22:14:18 -0500

    Update settings.py

--- image_parroter/image_parroter/settings.py
+++ image_parroter/image_parroter/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%uiuel=ss3=*g+t*=+lim@)+^mdrc8j&j^95sw&#=vd26$j3pu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
